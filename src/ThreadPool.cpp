63d6e78209e769e05a75c53487a516440ea60697
Anton Kashcheev <antonkachsheev@gmail.com>
2017-01-04 05:00:39 +0300

    Deleted useless things.

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,203 +1,80 @@
-#include <cassert>
-#include "Templates/RwLockQueueBuffer.hpp"
 #include "ThreadPool.hpp"
 
-struct ThreadPool::ProcessingTasks:
-		public RwLockQueueBuffer<AbstractTask *,
-				ThreadPool::BUFFER_PROCESSING_TASKS_SIZE>
-{
-};
-
-struct ThreadPool::FinishedTasks:
-		public RwLockQueueBuffer<AbstractTask *,
-				ThreadPool::BUFFER_FINISHED_TASKS_SIZE>
-{
-};
-
-void ThreadPool::Thread::run()
-{
-	static const types::Size MAX_COUNT_ITERATIONS_WAITING = 50;
-	Mutex internalMutex;
-	types::Size counter;
-	id.internal = &internalMutex;
-	while(!id.join->tryWait())
-	{
-		id.status = ThreadPool::ThreadStatus::READY;
-		if (id.processingTasks->size())
-		{
-			id.status = ThreadPool::ThreadStatus::PERFOMING;
-
-			internalMutex.lock();
-			(*id.processingTasks->head())();
-			internalMutex.unlock();
-
-			id.finishedTasks->push(id.processingTasks->pop());
-		}
-		else
-		{
-			if (counter < MAX_COUNT_ITERATIONS_WAITING)
-			{
-				++counter;
-			}
-			else
-			{
-				// sleep
-				internalMutex.lock();
-			}
-		}
-	}
-	id.status = ThreadPool::ThreadStatus::FINISHED;
-}
-
-ThreadPool::ThreadPool() :
-		lastId(0),
-		lastIdSemaphore(1),
-		threadCapacity(0),
-		arrayThread(nullptr),
-		blockedSemaphores(nullptr),
-		processingTasks(nullptr),
-		finishedTasks(nullptr)
+ThreadPool::ThreadPool()
 {
 }
 
-ThreadPool::ThreadPool(types::Size countThreads) : ThreadPool()
+ThreadPool::ThreadPool(types::Size countThreads) :
+		queueThreads(countThreads),
+		taskThreads(countThreads)
 {
-	init(countThreads);
 }
 
 ThreadPool::~ThreadPool()
 {
-	clear();
-}
-
-ThreadPool::TaskId ThreadPool::addTask(AbstractTask &task)
-{
-	using Size = types::Size;
-
-	types::Size countTasks = BUFFER_PROCESSING_TASKS_SIZE;
-	types::Size queueNumber = 0;
-	for (Size i = 0; i < threadCapacity; ++i)
+	for (types::Size i = 0; i != queueThreads.size(); ++i)
 	{
-		if (processingTasks[i].size() < countTasks) {
-			countTasks = processingTasks[i].size();
-			queueNumber = i;
-		}
+		queueThreads[i]->join();
 	}
 
-	return addTask(queueNumber, task);
+	for (types::Size i = 0; i != taskThreads.size(); ++i)
+	{
+		taskThreads[i]->join();
+	}
 }
 
-ThreadPool::TaskId ThreadPool::addTask(types::Size queueNumber, AbstractTask &task)
+TaskId ThreadPool::addTask(AbstractTask &task)
 {
 	TaskId taskId;
-	if (queueNumber < threadCapacity)
-	{
-		processingTasks[queueNumber].push(&task);
+	taskId.id = lastId++;
+	taskId.task = &task;
+	taskId.threadType = types::ThreadType::SIMPLE;
 
-		lastIdSemaphore.wait();
-		taskId.id = lastId++;
-		lastIdSemaphore.post();
+	return taskId;
+}
 
-		taskId.threadIndex = queueNumber;
-		taskId.task = &task;
-		taskId.pool = this;
-	}
+TaskId ThreadPool::addTask(types::Size threadNumber, AbstractTask &task)
+{
+	TaskId taskId;
+	taskId.id = lastId++;
+	taskId.task = &task;
+	taskId.threadType = types::ThreadType::QUEUE;
 
 	return taskId;
 }
 
 ThreadPool::TaskStatus ThreadPool::getTaskStatus(const TaskId &taskId) const
 {
-	using Size = types::Size;
 
-	if (taskId.pool && taskId.task && taskId.threadIndex < threadCapacity)
-	{
-		ProcessingTasks &processing = *(arrayThread[taskId.threadIndex].id.processingTasks);
-		for (Size i = 0; i < processing.size(); ++i)
-		{
-			if (taskId.task == processing[i])
-			{
-				if (i == 0)
-				{
-					return TaskStatus::PROCESS;
-				}
-				else
-				{
-					return TaskStatus::QUEUED;
-				}
-			}
-		}
-
-		FinishedTasks &finished = *(arrayThread[taskId.threadIndex].id.finishedTasks);
-		for (Size i = 0; i < finished.size(); ++i)
-		{
-			if (taskId.task == finished[i])
-			{
-				return TaskStatus::FINISHED;
-			}
-		}
-	}
-	else
-	{
-		return TaskStatus::ERROR;
-	}
-
-	return TaskStatus::MISSED;
-}
-
-void ThreadPool::setCountThreads(types::Size countThreads)
-{
-	assert(threadCapacity != 0);
-	init(countThreads);
 }
 
 void ThreadPool::start()
 {
-	using Size = types::Size;
 
-	for (Size i = 0; i != threadCapacity; ++i)
-	{
-		arrayThread->start();
-	}
 }
 
 void ThreadPool::stop()
 {
-	using Size = types::Size;
 
-	for (Size i = 0; i < threadCapacity; ++i)
-	{
-		arrayThread[i].id.join->post();
-		arrayThread[i].join();
-	}
 }
 
-void ThreadPool::init(types::Size countThreads)
+void ThreadPool::setCountTaskThreads(types::Size size)
 {
-	using Size = types::Size;
 
-	threadCapacity = countThreads;
-	arrayThread = new Thread[countThreads];
-	blockedSemaphores = new Semaphore[countThreads];
-	processingTasks = new ProcessingTasks[countThreads];
-	finishedTasks = new FinishedTasks[countThreads];
-
-	for (Size i = 0; i < threadCapacity; ++i)
-	{
-		blockedSemaphores[i].post();
-		arrayThread[i].id.join = blockedSemaphores + i;
-		arrayThread[i].id.parent = this;
-		arrayThread[i].id.processingTasks = processingTasks + i;
-		arrayThread[i].id.finishedTasks = finishedTasks + i;
-	}
 }
 
-void ThreadPool::clear()
+types::Size ThreadPool::getCountTaskThreads() const noexcept
 {
-	stop();
-
-	delete[] finishedTasks;
-	delete[] processingTasks;
-	delete[] blockedSemaphores;
-	delete[] arrayThread;
+	return taskThreads.size();
 }
+
+void ThreadPool::setCountQueueThreads(types::Size size)
+{
+
+}
+
+types::Size ThreadPool::getCountQueueThreads() const noexcept
+{
+	return queueThreads.size();
+}
+
--- src/ThreadPool.hpp
+++ src/ThreadPool.hpp
@@ -1,30 +1,21 @@
 #ifndef THREADPOOL_HPP
 #define THREADPOOL_HPP
 
-#include <cstdint>
-
 #include "Types.hpp"
 #include "AbstractTask.hpp"
 #include "Descriptor.hpp"
 #include "Semaphore.hpp"
 #include "Mutex.hpp"
-#include "Thread.hpp"
-#include "Templates/QueueBuffer.hpp"
+#include "QueueThread.hpp"
+#include "TaskThread.hpp"
+#include "Templates/UniquePointer.hpp"
+
+class TaskId;
 
 class ThreadPool
 {
 public:
-	enum class TaskStatus
-	{
-		ERROR,
-		MISSED,
-		QUEUED,
-		PROCESS,
-		FINISHED
-	};
-
-	class TaskId;
-
+	typedef QueueThread::TaskStatus TaskStatus;
 	typedef void*(*ThreadTaskType)(void *);
 
 	ThreadPool();
@@ -37,114 +28,26 @@ public:
 	ThreadPool &operator=(const ThreadPool &) = delete;
 
 	TaskId addTask(AbstractTask &task);
-	TaskId addTask(types::Size queueNumber, AbstractTask &task);
+	TaskId addTask(types::Size threadNumber, AbstractTask &task);
+
 	TaskStatus getTaskStatus(const TaskId &taskId) const;
 
-	void setCountThreads(types::Size size);
+	void setCountTaskThreads(types::Size size);
+	types::Size getCountTaskThreads() const noexcept;
+
+	void setCountQueueThreads(types::Size size);
+	types::Size getCountQueueThreads() const noexcept;
 
 	void start();
 	void stop();
 
-	// future
-	// bool stopThread(types::Size index);
-	// bool startThread(types::Size index);
-
 private:
-	enum Constants: types::Size
-	{
-		BUFFER_PROCESSING_TASKS_SIZE = 64,
-		BUFFER_FINISHED_TASKS_SIZE = 64
-	};
-
-	enum class ThreadStatus
-	{
-		NOT_RUN,
-		STARTING,
-		READY,
-		PERFOMING,
-		FINISHED
-	};
-
-	struct ProcessingTasks;
-	struct FinishedTasks;
-
-	class Thread: public ::Thread
-	{
-	protected:
-		virtual void run();
-
-		ThreadStatus getStatus();
-
-	private:
-		struct ThreadId
-		{
-			Descriptor descriptor;
-			Semaphore *join;
-			Mutex *internal;
-			ThreadPool *parent;
-			ProcessingTasks *processingTasks;
-			FinishedTasks *finishedTasks;
-
-			ThreadPool::ThreadStatus status;
-		} id;
-
-		friend class ThreadPool;
-	};
-
 	void init(types::Size countThreads);
 	void clear();
 
-	types::Size lastId;
-	Semaphore lastIdSemaphore;
-
-	types::Size threadCapacity;
-	Thread *arrayThread;
-	Semaphore *blockedSemaphores;
-	ProcessingTasks *processingTasks;
-	FinishedTasks *finishedTasks;
+	types::Uint64 lastId;
+	QueueBuffer<UniquePointer<QueueThread>, 0ul> queueThreads;
+	QueueBuffer<UniquePointer<TaskThread>, 0ul> taskThreads;
 };
-
-class ThreadPool::TaskId
-{
-public:
-	TaskId() : id(0), threadIndex(0), task(nullptr), pool(nullptr)
-	{
-	}
-
-	TaskId(ThreadPool::TaskId &&taskId) : id(taskId.id),
-			threadIndex(taskId.threadIndex),
-			task(taskId.task), pool(taskId.pool)
-	{
-	}
-	TaskId(const ThreadPool::TaskId &taskId) : id(taskId.id),
-			threadIndex(taskId.threadIndex),
-			task(taskId.task), pool(taskId.pool)
-	{
-	}
-
-	TaskId &operator=(const ThreadPool::TaskId &taskId);
-	TaskId &operator=(ThreadPool::TaskId &&taskId);
-
-private:
-	TaskId(types::Uint64 taskId, types::Size taskThreadIndex,
-			AbstractTask *thisTask, ThreadPool *parent) :
-			id(taskId), threadIndex(taskThreadIndex), task(thisTask), pool(parent)
-	{
-	}
-
-	types::Uint64 id;
-	types::Size threadIndex;
-	AbstractTask *task;
-	ThreadPool *pool;
-
-	friend class ThreadPool;
-	friend bool operator ==(const TaskId& taskId1, const TaskId& taskId2);
-	friend bool operator !=(const TaskId& taskId1, const TaskId& taskId2);
-};
-
-bool operator ==(const ThreadPool::TaskId& taskId1,
-		const ThreadPool::TaskId& taskId2);
-bool operator !=(const ThreadPool::TaskId& taskId1,
-		const ThreadPool::TaskId& taskId2);
 
 #endif // THREADPOOL_HPP
